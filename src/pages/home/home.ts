d76d5c83d0886d28e1b670c7d972605bb4d96a99
pereiraneto <pereira.neto.12@gmail.com>
2018-06-09 01:08:31 -0300

    App de Greeting criado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  personName: string;
 
+  constructor(public toast: ToastController) {
+
+  }
+
+  showGreeting(name: string){
+    this.toast.create({
+      message: `Hello ${name}`,
+      duration: 3000
+    }).present();
+    
   }
 
 }
